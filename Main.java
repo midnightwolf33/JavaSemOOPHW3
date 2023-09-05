7dbf79cec450af101385a3f21e3abd9ab651c068
Roman <125289934+midnightwolf33@users.noreply.github.com>
2023-09-05 13:30:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> lList = new LinkedList<Integer>();
+        lList.add(1);
+        lList.add(2);
+        lList.add(3);
+        System.out.println(lList);
+    }
+}
